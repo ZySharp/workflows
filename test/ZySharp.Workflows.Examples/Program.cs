abb9752b41f6edaeae372f9fda3a0ba34009a64e
Florian Bernd <git@flobernd.de>
2022-12-10 17:01:53 +0100

    Add test for CI workflow (#4)

---
+++ test/ZySharp.Workflows.Examples/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ZySharp.Workflows.Examples;
+
+internal static class Program
+{
+    [ExcludeFromCodeCoverage]
+    private static void Main()
+    {
+        throw new NotImplementedException();
+    }
+}
